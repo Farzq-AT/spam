77021552cc93ee54ae94e154ab2bffab0a1aa783
Farzq-AT <79454101+Farzq-AT@users.noreply.github.com>
2021-02-25 22:35:29 +0700

    Add files via upload

---
+++ loading.py
@@ -0,0 +1,7 @@
+import os, sys, threading
+
+from warn.warn import *
+
+def loding():
+
+    print (f"{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}{Ah}•{Ih}•{kon}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}•{Ih}•{Ah}")
